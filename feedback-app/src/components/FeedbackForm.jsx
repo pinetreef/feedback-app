f71aa6186330ff723e54247e1f4b9d004b0216a4
Fabio <fabio.pinheiro@hotmail.com>
2023-03-01 19:56:17 -0300

    Section 4: Rating Select Component [DONE]

--- feedback-app/src/components/FeedbackForm.jsx
+++ feedback-app/src/components/FeedbackForm.jsx
@@ -28,7 +28,7 @@ function FeedbackForm() {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect />
+        <RatingSelect select={(rating) => setRating(rating)}/>
         <div className="input-group">
           <input onChange={handleTextChange} type='text' placeholder='Write a review' value={text} />
           <Button type='submit' isDisabled={btnDisabled}>Send</Button>
