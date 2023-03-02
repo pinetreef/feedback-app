a7160b910bda4ec6c42982be0c9aaaede1525b96
Fabio <fabio.pinheiro@hotmail.com>
2023-03-01 21:59:37 -0300

    Section 4: Fade Animation with Framer Motion.

--- feedback-app/src/components/FeedbackList.jsx
+++ feedback-app/src/components/FeedbackList.jsx
@@ -1,4 +1,4 @@
-
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from './FeedbackItem'
 function FeedbackList({feedback, handleDelete}) {
@@ -8,12 +8,32 @@ function FeedbackList({feedback, handleDelete}) {
 
     return (
     <div className='feedback-list'>
+        <AnimatePresence>
+        {feedback.map((item) => (
+            <motion.div
+            key={item.id}
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            exit={{opacity: 0}}
+            >
+            <FeedbackItem key={item.id} item={item}
+            handleDelete={handleDelete} />
+            </motion.div>
+        ))}
+        </AnimatePresence>
+    </div>
+  )
+
+  /*Feedback list return withou animation
+    return (
+    <div className='feedback-list'>
         {feedback.map((item) => (
             <FeedbackItem key={item.id} item={item}
             handleDelete={handleDelete} />
         ))}
     </div>
   )
+  */
 }
 
 FeedbackList.propTypes = {
